0055b427791068bea7eb2e2470de55f4680780ad
Dahman <94912743+DahmanCode@users.noreply.github.com>
2023-05-18 17:32:50 +0100

    add a header and main components

---
+++ meme-generator/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return(
+    <nav className="navbar">
+      <img src="images/logo.png" />
+      <h4>React Course - Project 3</h4>
+    </nav>
+  )
+}
+
+export default Header
