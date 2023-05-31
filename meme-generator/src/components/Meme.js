31cc2f99fcaa8a63dffda836f4be53bb688c6055
Dahman <94912743+DahmanCode@users.noreply.github.com>
2023-05-31 01:40:31 +0100

    get memes data from an API and use effect Hook to fetch it

--- meme-generator/src/components/Meme.js
+++ meme-generator/src/components/Meme.js
@@ -1,17 +1,25 @@
-import React, {useState} from "react";
-import memesData from "../data/memesData"
+import React, {useState, useEffect} from "react";
+// import memesData from "../data/memesData"
 
 function Meme() {
   
   const [meme, setMeme] = useState({
     topText: "",
     bottomText: "",
-    randomImg: "https://i.imgflip.com/24y43o.jpg"
+    randomImg: "https://i.imgflip.com/1bij.jpg"
   })
+
+  const [allMemes, setAllMemes] = useState([])
+
+  useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+      .then(res => res.json)
+      .then(data => setAllMemes(data.data.memes))
+  }, [])
   
   function getMemeImg() {
-    const memesArray = memesData.data.memes
-    const randomURL = memesArray[Math.floor(Math.random() * memesArray.length)].url
+    // const memesArray = memesData.data.memes
+    const randomURL = allMemes[Math.floor(Math.random() * allMemes.length)].url
     setMeme(prevMem => ({
       ...prevMem,
       randomImg: randomURL
