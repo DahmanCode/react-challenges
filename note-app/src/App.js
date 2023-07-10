781c337e6b78db95924fdc94d8016f4c73182423
Dahman <94912743+DahmanCode@users.noreply.github.com>
2023-07-10 20:18:00 +0100

    sort notes

--- note-app/src/App.js
+++ note-app/src/App.js
@@ -14,6 +14,8 @@ export default function App() {
     const currentNote = 
         notes.find(note => note.id === currentNoteId) 
         || notes[0]
+    
+    const sortedNotes = notes.sort((a, b) => b.updatedAt - a.updatedAt)
 
     useEffect(() => {
         const unsubscribe = onSnapshot(notesCollection, function(snapshot) {
@@ -68,7 +70,7 @@ export default function App() {
                 className="split"
             >
                 <Sidebar
-                    notes={notes}
+                    notes={sortedNotes}
                     currentNote={currentNote}
                     setCurrentNoteId={setCurrentNoteId}
                     newNote={createNewNote}
