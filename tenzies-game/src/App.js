e9f9dafebd02a9331133c0aea804e87b396402cf
Dahman <94912743+DahmanCode@users.noreply.github.com>
2023-07-14 20:30:40 +0100

    Hold dice part

--- tenzies-game/src/App.js
+++ tenzies-game/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import Die from './Die'
+import { nanoid } from 'nanoid'
 
 import './style.css'
 
@@ -7,21 +8,56 @@ function App() {
 
   const [dice, setDice] = useState(allNewDice())
 
+  function generateNewDie() {
+    return {
+      value: Math.ceil(Math.random() * 9), 
+      isHeld: false,
+      id: nanoid()
+    }
+  }
+
   function allNewDice() {
     const newDice = []
     for (let i = 0; i < 10; i++) {
-      newDice.push(Math.ceil(Math.random() * 9))
+      newDice.push(generateNewDie())
     }
     return newDice
   }
 
-  const diceEl = dice.map(die => <Die value={die} />)
+  function rollDice() {
+    setDice(oldDice => oldDice.map(die => {
+      return die.isHeld ?
+      die :
+      generateNewDie()
+    }))
+  }
+
+  function holdDice(id) {
+    setDice(oldDice => oldDice.map(die => {
+      return die.id === id ?
+       {...die, isHeld: !die.isHeld} :
+       die
+    }))
+  }
+
+  const diceEl = dice.map(die => (
+    <Die 
+      key={die.id} 
+      value={die.value} 
+      isHeld={die.isHeld} 
+      holdDice={() => holdDice(die.id)}
+    />
+  ))
 
   return (
     <main>
+      <h1 className="title">Tenzies</h1>
+        <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className='dice-container'>
         {diceEl}
       </div>
+
+      <button className='roll-dice' onClick={rollDice}>Roll</button>
     </main>
   )
 }
