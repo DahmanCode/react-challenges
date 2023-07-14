909128d9976e3a73f032df4d33d7093a46e26099
Dahman <94912743+DahmanCode@users.noreply.github.com>
2023-07-14 02:25:10 +0100

    Die Components

--- tenzies-game/src/App.js
+++ tenzies-game/src/App.js
@@ -1,11 +1,24 @@
 import React, { useState, useEffect } from 'react'
+import Die from './Die'
+
 import './style.css'
 
 function App() {
 
   return (
     <main>
-      
+      <div className='dice-container'>
+        <Die value="1"/>
+        <Die value="1"/>
+        <Die value="1"/>
+        <Die value="1"/>
+        <Die value="1"/>
+        <Die value="1"/>
+        <Die value="1"/>
+        <Die value="1"/>
+        <Die value="1"/>
+        <Die value="1"/>
+      </div>
     </main>
   )
 }
