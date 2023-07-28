b7a5b35ed6f8cb86b8ec294bb0a6cfe9760c1905
Dahman <94912743+DahmanCode@users.noreply.github.com>
2023-07-28 12:16:05 +0100

    change Link to NavLink

--- van-life-app/src/components/Header.jsx
+++ van-life-app/src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 import Logo from "../assets/vanLogo.png"
 
@@ -10,9 +10,24 @@ function Header() {
         <img src={Logo} width={85} alt="logo" />
       </Link>
       <nav>
-        <Link to="/host">Host</Link>
-        <Link to="/about">About</Link>
-        <Link to="/vans">Vans</Link>
+        <NavLink 
+          className={({isActive}) => isActive ? "active-link" : null} 
+          to="/host"
+        >
+          Host
+        </NavLink>
+        <NavLink 
+          className={({isActive}) => isActive ? "active-link" : null} 
+          to="/about"
+        >
+          About
+        </NavLink>
+        <NavLink 
+          className={({isActive}) => isActive ? "active-link" : null} 
+          to="/vans"
+        >
+          Vans
+        </NavLink>
       </nav>
     </header>
   )
