5bdfcf22ae3b1fb790dd41e681d27891ef3fda90
Dahman <94912743+DahmanCode@users.noreply.github.com>
2023-08-07 19:19:07 +0100

    change some paddings errors

--- van-life-app/src/App.jsx
+++ van-life-app/src/App.jsx
@@ -46,7 +46,7 @@ function App() {
 
           </Route>
 
-          <Route path="*" element={<NotFound />} />
+          <Route path="/*" element={<NotFound />} />
         </Route>
 
       </Routes>
